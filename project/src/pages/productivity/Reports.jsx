792716a1440ab7a27ee487ab41cdb3a342ec8aa1
khandaniyal14-lab <daniyal@irp.edu.pk>
2025-10-15 20:29:19 +0500

    Update Reports.jsx

--- project/src/pages/productivity/Reports.jsx
+++ project/src/pages/productivity/Reports.jsx
@@ -35,7 +35,7 @@ const Reports = () => {
   const token = localStorage.getItem("token");
 
   const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://productix-backend-v1rk.onrender.com",
     headers: { Authorization: `Bearer ${token}` },
   });
 
