92183e2f804ab5b016369a94d9053b574b263fcd
khandaniyal14-lab <daniyal@irp.edu.pk>
2025-10-15 12:52:49 +0500

    Update database.py

--- productix_fastapi/app/database.py
+++ productix_fastapi/app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg2://postgres:king1234rootcar@db.jdzgzudaphddkkkwhubf.supabase.co:5432/postgres?sslmode=require
-"
+DATABASE_URL = "postgresql+psycopg2://postgres:king1234rootcar@db.jdzgzudaphddkkkwhubf.supabase.co:5432/postgres?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
